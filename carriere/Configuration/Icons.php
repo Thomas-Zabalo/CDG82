15570593fd5ca1410db0ff0336387fe2f8d26f7d
Thomas ZABALO <150163380+Thomas-Zabalo@users.noreply.github.com>
2025-03-14 17:00:52 +0100

    Delete carriere directory

--- carriere/Configuration/Icons.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-   'example' => [
-      'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-      'source' => 'EXT:carriere/Resources/Public/Icons/SimpleCard.svg',
-   ],
-];
